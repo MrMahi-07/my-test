bff30f5e939e7d8f5bb2c4418f562f6ac7e65b14
Mahesh <mrmahi353@gmail.com>
2023-03-30 04:11:39 +0530

    handles games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 function getCroppedImageUrl(url: string) {
-	if (!url) return "";
+	if (!url) return noImage;
 	let index = url.indexOf("media/") + 6;
 	let updatedUrl =
 		url.substring(0, index) + "crop/600/400/" + url.substring(index);
