4de48d095b669acce0b75648251d1c359e175371
Mahesh <mrmahi353@gmail.com>
2023-03-29 13:53:33 +0530

    install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/button";
 import React from "react";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<Button colorScheme={"blue"} m="3">
+			Submit
+		</Button>
+	);
 }
 
 export default App;
