65b4fa286927678a25d47e20d0444b45fb374224
Mahesh <mrmahi353@gmail.com>
2023-03-30 04:08:08 +0530

    sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,40 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
-function SortSelector() {
+interface Props {
+	onSelectSortOrder: (sortOrder: string) => void;
+	sortOrder: string;
+}
+
+function SortSelector({ onSelectSortOrder, sortOrder }: Props) {
+	const sortOrders = [
+		{ key: 1, value: "", label: "Relevance" },
+		{ key: 3, value: "-added", label: "Date added" },
+		{ key: 4, value: "", label: "Name" },
+		{ key: 5, value: "-released", label: "Release date" },
+		{ key: 6, value: "-metacritic", label: "Popularity" },
+		{ key: 7, value: "-rating", label: "Average rating" },
+	];
+
+	const currentSortOrder = sortOrders.find(
+		(order) => order.value === sortOrder
+	);
+
 	return (
 		<Menu>
 			<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-				Order by: Relevance
+				Order by: {currentSortOrder?.label || "Relevance"}
 			</MenuButton>
 			<MenuList>
-				<MenuItem>Relevance</MenuItem>
-				<MenuItem>Date added</MenuItem>
-				<MenuItem>Name</MenuItem>
-				<MenuItem>Release date</MenuItem>
-				<MenuItem>Popularity</MenuItem>
-				<MenuItem>Average rating</MenuItem>
+				{sortOrders.map((order) => (
+					<MenuItem
+						onClick={() => onSelectSortOrder(order.value)}
+						key={order.key}
+						value={order.value}
+					>
+						{order.label}
+					</MenuItem>
+				))}
 			</MenuList>
 		</Menu>
 	);
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 function getCroppedImageUrl(url: string) {
+	if (!url) return "";
 	let index = url.indexOf("media/") + 6;
 	let updatedUrl =
 		url.substring(0, index) + "crop/600/400/" + url.substring(index);
