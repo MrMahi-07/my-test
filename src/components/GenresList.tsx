ea9f452f3d3dbb5efbe7a00ccd5694d5c456a9ec
Mahesh <mrmahi353@gmail.com>
2023-03-29 20:43:35 +0530

    added generic use

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,13 +1,12 @@
 import { List } from "@chakra-ui/react";
-import React from "react";
 import useGenres from "../hooks/useGenres";
 
 function GenresList() {
-	const { genres } = useGenres();
+	const { data } = useGenres();
 	return (
 		<List>
-			{genres.map((genre) => (
-				<li key={genre.id}>{genre.name}</li>
+			{data.map((d) => (
+				<li key={d.id}>{d.name}</li>
 			))}
 		</List>
 	);
